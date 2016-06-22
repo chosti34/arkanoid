0faf01d569de41ab2a309f6f2a4e27615d67d364
chosti34 <chosti34@gmail.com>
2016-06-22 14:38:42 +0300

    Для смены названия файла

--- php/save_database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once '../include/database.inc.php';
-
-    $name = trim(strip_tags($_POST['user']));
-    $score  = trim(strip_tags($_POST['score']));
-
-    $query = "INSERT INTO players(name, score) VALUES('$name', $score)";
-
-    mysql_query($query);
