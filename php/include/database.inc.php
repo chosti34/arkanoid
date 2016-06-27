5e7b37fa760324dbc65a157140482e387024d742
chosti34 <chosti34@gmail.com>
2016-06-28 01:11:10 +0300

    Добавлено скрытие warning'а (через символ @)

--- php/include/database.inc.php
+++ php/include/database.inc.php
@@ -1,7 +1,7 @@
 <?php
     require_once 'config.inc.php';
 
-    $g_dbLink = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+    $g_dbLink = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
     $error = mysqli_connect_error();
     if ($error)
