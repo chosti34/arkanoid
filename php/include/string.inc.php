4d5e6f1f278c906808461f3e4870ca435be6390a
chosti34 <chosti34@gmail.com>
2016-07-08 13:47:51 +0300

    Файл пока не нужен

--- php/include/string.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-    function formStringOfTopPlayer($id, $name, $score)
-    {
-        return $id . ', ' . htmlspecialchars(stripslashes($name)) . ', ' . htmlspecialchars(stripslashes($score)) . '<br />';
-    }
