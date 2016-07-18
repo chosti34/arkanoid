7007218f4949e2d25916564680bf2b4e46ee8bde
chosti34 <chosti34@gmail.com>
2016-07-18 17:09:01 +0300

    Для смены структуры проекта

--- js/Brick.js
+++
@@ -1,9 +0,0 @@
-function Brick(x, y, width, height, fillColor, strokeColor)
-{
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.fillColor = fillColor;
-    this.strokeColor = strokeColor;
-}
